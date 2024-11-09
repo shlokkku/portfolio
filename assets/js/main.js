6593846a80bd085ed6e023ca77f499dda1de5693
shlokkku <149516532+shlokkku@users.noreply.github.com>
2024-11-10 02:14:44 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+function openTab(evt, elmnt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("qualification-icon-active", " qualification-icon");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " qualification-icon-active";
+}
+
+document.getElementById("defaultOpen").click();
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function(form) {
+            form.addEventListener('submit', function(event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
